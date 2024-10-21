9dbccffa9c957f95b97dfd75c14e35c0ce3d9c07
yu-yama <yu-yama@users.noreply.github.com>
2024-10-21 13:58:59 +0100

    chore: instantiate `FunLike` and `@[ext]` lemmas on cocycles

--- GroupExtension/Deps/LowDegree.lean
+++ GroupExtension/Deps/LowDegree.lean
@@ -5,29 +5,27 @@ namespace groupCohomology
 universe u
 variable {k : Type u} {G : Type u} [CommRing k] [Group G] {A : Rep k G}
 
-lemma oneCocycles_ext_iff (f₁ f₂ : oneCocycles A) :
-    f₁ = f₂ ↔ ∀ g : G, f₁.1 g = f₂.1 g :=
-  ⟨fun h g ↦ congrFun (congrArg Subtype.val h) g, fun h ↦ Subtype.ext (funext fun g ↦ h g)⟩
+instance : FunLike (oneCocycles A) G A := ⟨(·.val), Subtype.val_injective⟩
 
-lemma mem_oneCoboundaries_iff (f : oneCocycles A) :
-    f ∈ oneCoboundaries A ↔
-    ∃ x : A, ∀ g : G, A.ρ g x - x = f.1 g := by
-  apply exists_congr
-  intro x
-  simp only [LinearMap.codRestrict, dZero, LinearMap.coe_mk, AddHom.coe_mk, oneCocycles_ext_iff]
+@[ext]
+theorem oneCocycles_ext {f₁ f₂ : oneCocycles A} (h : ∀ g : G, f₁ g = f₂ g) : f₁ = f₂ :=
+  DFunLike.ext f₁ f₂ h
 
-lemma twoCocycles_ext_iff (f₁ f₂ : twoCocycles A) :
-    f₁ = f₂ ↔
-    ∀ g h : G, f₁.1 (g, h) = f₂.1 (g, h) :=
-  ⟨fun hf g h ↦ congrFun (congrArg Subtype.val hf) (g, h),
-    fun hf ↦ Subtype.ext (funext fun ⟨g, h⟩ ↦ hf g h)⟩
+instance : FunLike (twoCocycles A) (G × G) A := ⟨(·.val), Subtype.val_injective⟩
 
-lemma mem_twoCoboundaries_iff (f : twoCocycles A) :
-    f ∈ twoCoboundaries A ↔
-    ∃ x : G → A, ∀ g h : G, A.ρ g (x h) - x (g * h) + x g = f.1 (g, h) := by
-  apply exists_congr
-  intro x
-  simp only [LinearMap.codRestrict, dOne, LinearMap.coe_mk, AddHom.coe_mk, twoCocycles_ext_iff]
+@[ext]
+theorem twoCocycles_ext {f₁ f₂ : twoCocycles A} (h : ∀ g h : G, f₁ (g, h) = f₂ (g, h)) : f₁ = f₂ :=
+  DFunLike.ext f₁ f₂ (Prod.forall.mpr h)
+
+theorem mem_oneCoboundaries_iff (f : oneCocycles A) : f ∈ oneCoboundaries A ↔
+    ∃ x : A, ∀ g : G, A.ρ g x - x = f g := exists_congr fun x ↦ by
+  simpa only [LinearMap.codRestrict, dZero, LinearMap.coe_mk, AddHom.coe_mk] using
+    groupCohomology.oneCocycles_ext_iff
+
+theorem mem_twoCoboundaries_iff (f : twoCocycles A) : f ∈ twoCoboundaries A ↔
+    ∃ x : G → A, ∀ g h : G, A.ρ g (x h) - x (g * h) + x g = f (g, h) := exists_congr fun x ↦ by
+  simpa only [LinearMap.codRestrict, dOne, LinearMap.coe_mk, AddHom.coe_mk] using
+    groupCohomology.twoCocycles_ext_iff
 
 end groupCohomology
 
@@ -35,8 +33,8 @@ end groupCohomology
 example {G A : Type} [Group G] [CommGroup A] [MulDistribMulAction G A]
     {f f' : groupCohomology.twoCocycles (Rep.ofMulDistribMulAction G A)}
     (h : f - f' ∈ groupCohomology.twoCoboundaries (Rep.ofMulDistribMulAction G A)) :
-    ∃ x : G → A, ∀ g₁ g₂ : G, Additive.toMul (α := A) (f.1 (g₁, g₂)) *
-    (Additive.toMul (α := A) (f'.1 (g₁, g₂)))⁻¹ = g₁ • x g₂ * (x (g₁ * g₂))⁻¹ * x g₁ := by
+    ∃ x : G → A, ∀ g₁ g₂ : G, Additive.toMul (α := A) (f (g₁, g₂)) *
+    (Additive.toMul (α := A) (f' (g₁, g₂)))⁻¹ = g₁ • x g₂ * (x (g₁ * g₂))⁻¹ * x g₁ := by
   rw [groupCohomology.mem_twoCoboundaries_iff] at h
   obtain ⟨x, hx⟩ := h
   use fun g ↦ Additive.toMul (x g)
