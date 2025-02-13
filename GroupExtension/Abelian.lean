a488e95e2210dbf6003c8384d5721f781808b5a3
yu-yama <yu-yama@users.noreply.github.com>
2025-02-13 16:35:55 +0000

    style: use camel case in definitions

--- GroupExtension/Abelian.lean
+++ GroupExtension/Abelian.lean
@@ -11,7 +11,7 @@ group extensions in general, see `Mathlib/GroupTheory/GroupExtension/Basic.lean`
 
 ## Main definitions
 
-- `SemidirectProduct.conjClasses_equiv_h1 (φ : G →* MulAut N)`: the bijection between the
+- `SemidirectProduct.conjClassesEquivH1 (φ : G →* MulAut N)`: the bijection between the
   `N`-conjugacy classes of splittings associated to the semidirect product `G ⋊[φ] N` and
   $H^1 (G, N)$
 - `GroupExtension.ofMulDistribMulAction.equivH2 [MulDistribMulAction G N]` : the bijection between
@@ -35,7 +35,7 @@ theorem toRep_ρ_apply_apply (g : G) (n : Additive N) :
     ((toRep φ).ρ g) n = Additive.ofMul (φ g (Additive.toMul n)) := rfl
 
 /-- Returns the 1-cocycle corresponding to a splitting. -/
-def splitting_toOneCocycle (s : (toGroupExtension φ).Splitting) :
+def splittingToOneCocycle (s : (toGroupExtension φ).Splitting) :
     groupCohomology.oneCocycles (toRep φ) where
   val g := Additive.ofMul (α := N) (s g).left
   property := by
@@ -44,7 +44,7 @@ def splitting_toOneCocycle (s : (toGroupExtension φ).Splitting) :
     rw [toRep_ρ_apply_apply, toMul_ofMul, map_mul, mul_left, mul_comm, ← ofMul_mul, right_splitting]
 
 /-- Returns the splitting corresponding to a 1-cocycle. -/
-def splitting_ofOneCocycle (f : groupCohomology.oneCocycles (toRep φ)) :
+def splittingOfOneCocycle (f : groupCohomology.oneCocycles (toRep φ)) :
     (toGroupExtension φ).Splitting where
   toFun g := ⟨Additive.toMul (f g), g⟩
   map_one' := by
@@ -61,18 +61,18 @@ def splitting_ofOneCocycle (f : groupCohomology.oneCocycles (toRep φ)) :
 
 /-- The bijection between the splittings of the group extension associated to a semidirect product
   and the 1-cocycles -/
-def splitting_equiv_oneCocycles :
+def splittingEquivOneCocycles :
     (toGroupExtension φ).Splitting ≃ groupCohomology.oneCocycles (toRep φ) where
-  toFun := splitting_toOneCocycle φ
-  invFun := splitting_ofOneCocycle φ
+  toFun := splittingToOneCocycle φ
+  invFun := splittingOfOneCocycle φ
   left_inv s := by
-    unfold splitting_toOneCocycle splitting_ofOneCocycle
+    unfold splittingToOneCocycle splittingOfOneCocycle
     congr
     ext g
     <;> congr
     <;> exact (s.rightHom_splitting g).symm
   right_inv f := by
-    unfold splitting_toOneCocycle splitting_ofOneCocycle
+    unfold splittingToOneCocycle splittingOfOneCocycle
     ext g
     simp only [mk_eq_inl_mul_inr, GroupExtension.Splitting.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk,
       mul_left, left_inl, right_inl, map_one, left_inr, MulAut.one_apply, mul_one, ofMul_toMul,
@@ -82,7 +82,7 @@ def splitting_equiv_oneCocycles :
   1-coboundary. -/
 theorem isConj_iff_sub_mem_oneCoboundaries (s₁ s₂ : (toGroupExtension φ).Splitting) :
     (toGroupExtension φ).IsConj s₁ s₂ ↔
-    splitting_toOneCocycle φ s₁ - splitting_toOneCocycle φ s₂ ∈
+    splittingToOneCocycle φ s₁ - splittingToOneCocycle φ s₂ ∈
     groupCohomology.oneCoboundaries (toRep φ) := by
   rw [sub_mem_comm_iff, groupCohomology.mem_oneCoboundaries_iff]
   apply Equiv.exists_congr Additive.ofMul
@@ -96,12 +96,12 @@ theorem isConj_iff_sub_mem_oneCoboundaries (s₁ s₂ : (toGroupExtension φ).Sp
   rw [eq_mul_inv_iff_mul_eq, mul_comm, ← eq_mul_inv_iff_mul_eq, mul_assoc, mul_comm,
     ← mul_inv_eq_iff_eq_mul, ← groupCohomology.oneCocycles.val_eq_coe, AddSubgroupClass.coe_sub,
     Pi.sub_apply]
-  simp only [splitting_toOneCocycle, toRep_ρ_apply_apply, toMul_ofMul, ← div_eq_mul_inv]
+  simp only [splittingToOneCocycle, toRep_ρ_apply_apply, toMul_ofMul, ← div_eq_mul_inv]
   rfl
 
 /-- The bijection between the `N`-conjugacy classes of splittings and the first cohomology group -/
-def conjClasses_equiv_h1 : (toGroupExtension φ).ConjClasses ≃ groupCohomology.H1 (toRep φ) :=
-  Quotient.congr (splitting_equiv_oneCocycles φ) (by
+def conjClassesEquivH1 : (toGroupExtension φ).ConjClasses ≃ groupCohomology.H1 (toRep φ) :=
+  Quotient.congr (splittingEquivOneCocycles φ) (by
     intro s₁ s₂
     rw [Submodule.quotientRel_def]
     exact isConj_iff_sub_mem_oneCoboundaries φ s₁ s₂
